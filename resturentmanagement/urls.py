224510d877c4a9c911b5ca03df83309dbfdd5a4a
Koushik <chattarajkoushik@gmail.com>
2021-11-30 20:30:23 +0530

    update urls

--- resturentmanagement/urls.py
+++ resturentmanagement/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls.conf import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("resturent.urls")),
+    re_path (r'^media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT}),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
