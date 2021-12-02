b3113a2918d28c80d0d4ae2076e01989883f2115
Koushik <chattarajkoushik@gmail.com>
2021-12-02 14:38:34 +0530

    work on userlist

--- resturent/urls.py
+++ resturent/urls.py
@@ -11,5 +11,9 @@ urlpatterns = [
     path('sign-in',views.SigninView.as_view(),name="signinview"),
     path('sign-up',views.SignupView.as_view(),name="signupview"),
     path('profile',views.AccountView.as_view(),name="accountview"),
+    path('profile/<str:pk>',views.ProfileView.as_view(),name="profileview"),
+    path('logout',views.LogoutView.as_view(),name="logoutview"),
+    path('post-feed',views.PostFeedView.as_view(),name="PostFeedView"),
+    path('user-list',views.AllUserView.as_view(),name="alluserview"),
     path('signupajax',views.SignUpajaxView.as_view(),name="signupajaxview"),
 ]
--- resturent/views.py
+++ resturent/views.py
@@ -1,14 +1,17 @@
+from django import http
 from django.http.response import JsonResponse
 from django.shortcuts import redirect, render
 from django.http import HttpResponse, request
 from django.views.generic.base import View
-from django.contrib.auth.models import User, auth
-from resturent.models import UserProfile
+from django.contrib.auth.models import AnonymousUser, User, auth
+from resturent.models import Feed, UserProfile
 import string
 import random
 from django.contrib import messages
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
 
 
 
@@ -69,7 +72,6 @@ class SigninView(View):
             auth.login(request, user)
             return redirect ('/profile')
         else:
-            print('failed to login')
             messages.warning(request,"Invalid Creditional!!!!!!!!!!")
             return redirect ('/sign-in')
             # No backend authenticated the credentials
@@ -110,9 +112,11 @@ class SignupView(View):
             user.set_password(password)
             user.save()
         except Exception as e:
-            print(e)
+            messages.warning(request,e)
+            return redirect('/sign-up')
+
         try:
-            userprofile = UserProfile.objects.create(user=user,name=name,phoneno=phone_no,reffer_by=refferal,address=address,pincode=pincode,refferal_code=refferal_code(),otp=send_otp(),is_active=True,token = token_generator(),dpimage='/profile/usericon.png')
+            userprofile = UserProfile.objects.create(user=user,name=name,phoneno=phone_no,email=email,reffer_by=refferal,address=address,pincode=pincode,refferal_code=refferal_code(),otp=send_otp(),is_active=True,token = token_generator(),dpimage='/profile/usericon.png',role='user')
             # print(userprofile.otp)
             # if userprofile.otp == otp:
             #     userprofile.is_active = True
@@ -140,16 +144,20 @@ class AccountView(View):
     template_name = 'account.html'
     
     def get(self, request):
-        # try:
-        userprofile = request.user.userprofile
-        print('user-dp',request.user.userprofile.dpimage)
-        context = {'user':userprofile}
-        if request.user.is_authenticated:
-            return render(request, self.template_name, context)
-        else:
-            return redirect('/sign-in')
-        # except :
-        #     return redirect('/sign-in')
+        
+        try:
+            if request.user.is_authenticated:
+                userprofile = request.user.userprofile
+                newsfeed = Feed.objects.filter(user=request.user)
+                context = {
+                    'userprofile':userprofile,
+                    'newsfeed':newsfeed
+                    }
+                return render(request, self.template_name, context)
+            else:
+                return redirect('/sign-in')
+        except Exception as e:
+            return redirect('/')
 
     def post(self,request):
         name = request.POST.get("name")
@@ -160,6 +168,7 @@ class AccountView(View):
         phoneno = request.POST.get("phoneno")
         image = request.FILES.get("profilephoto")
         user = UserProfile.objects.get(phoneno=phoneno)
+
         if name:
             user.name = name
         if address:
@@ -172,7 +181,65 @@ class AccountView(View):
             user.bio = bio
         if image:
             user.dpimage = image
+
         user.save()
+        
 
         messages.success(request,"Your Profile is Sucessfully Updated!")
         return redirect("/profile")
+
+class PostFeedView(View):
+
+    def post(self,request):
+        user = User.objects.get(username=request.user)
+        feed = Feed.objects.create(user=user)
+        postimage = request.FILES.get("postimage")
+        title = request.POST.get("title")
+        des = request.POST.get("des")
+        if postimage:
+            feed.postimage = postimage
+        if title:
+            feed.title = title
+        if des:
+            feed.descrption = des
+        feed.save()
+        messages.success(request,"Post Created!")
+        return redirect("/profile")
+
+class AllUserView(View):
+
+    template_name = 'profilelist.html'
+
+    def get(self, request):
+        if request.user.is_authenticated:
+            userlist = UserProfile.objects.filter(role="user").exclude(user=request.user)
+        else:
+            userlist = UserProfile.objects.filter(role="user")
+
+        context = {
+            'userlist':userlist
+        }
+        return render(request, self.template_name, context)
+
+class LogoutView(View):
+
+    def get(self, request):
+        logout(request)
+        return redirect ("/")
+
+class ProfileView(View):
+    template_name = 'profile.html'
+
+
+    def get(self,request,pk):
+        userprofile = UserProfile.objects.get(id=pk)
+        user = User.objects.get(username = userprofile.phoneno )
+        feed = Feed.objects.filter(user=user)
+        userobj = UserProfile.objects.filter(id=pk).first()
+        context = {
+            'userobj':userobj,
+            'feedobj':feed
+        }
+        return render(request, self.template_name, context)
+
+
